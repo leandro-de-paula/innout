072ac75dcef8cf06d98d427e6d766745fbcc6a16
Leandro de Paula <leandrodepaula.ti@gmail.com>
2023-03-09 05:33:09 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,8 @@
 <?php
 require_once(dirname(__FILE__, 2 ) . "/src/config/config.php");
 $uri = urldecode($_SERVER['REQUEST_URI']);
-echo $uri;
-if (
-        $uri === '/' || 
-        $uri === '' || 
-        $uri === "/index.php" ||
-        $uri === "/innout/public/" ||
-        $uri === "/innout/public/index.php"
-    ) {
+
+if ($uri === '/' || $uri === '' ||  $uri === "/index.php" ) {
     $uri = '/login.php';
 } 
 require_once(CONTROLLER_PATH . "/{$uri}");
