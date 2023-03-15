4467317cf7cb8d15cdcf13bfb0e17d4eead355db
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-03-15 07:09:53 -0300

    Working the menu sidebar

---
+++ src/views/template/left.php
@@ -0,0 +1,31 @@
+<aside class="sidebar">
+    <nav class="menu mt-3">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href="day_records.php">
+                    <i class="icofont-ui-check mr-2"></i>
+                    Registrar Ponto
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="day_records.php">
+                    <i class="icofont-ui-calendar mr-2"></i>
+                    Registrar Mensal
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-chart-histogram mr-2"></i>
+                    Registrar Gerencial
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-users mr-2"></i>
+                    Usuários
+                </a>
+            </li>
+        </ul>
+    </nav>
+    
+</aside>
