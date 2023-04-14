5fab2acbbea1c5f518858826f783b1c83e8f3920
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-04-14 05:30:01 -0300

    DEBUG --> http://localhost/data_generator.php

--- extras/db2.sql
+++ extras/db2.sql
@@ -1,5 +1,5 @@
 SELECT *
   FROM `INNOUT`.working_hours;
 
-INSERT INTO `INNOUT`.working_hours (id,user_id,work_date,time1,time2,time3,time4,worked_time) 
-    VALUES (,1,'2023-04-03','08:00:00','12:00:00','13:00:00','17:00:00',28800)
+INSERT INTO `INNOUT`.working_hours (user_id,work_date,time1,time2,time3,time4,worked_time) 
+    VALUES (1,'2023-04-03','08:00:00','12:00:00','13:00:00','17:00:00',28800);
