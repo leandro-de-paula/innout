c7a13a508f41770d0aa24124d803a54ee8efd7f7
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-05-02 21:28:14 -0300

    Part 1 - Clock In

--- src/controllers/innout.php
+++ src/controllers/innout.php
@@ -7,7 +7,7 @@ loadModel('WorkingHours');
 $user = $_SESSION['user'];
 $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
-$currentTime = strftime('%H:%M:%S', time());
+$currentTime = date('H:i:s', time());
 $records->innout($currentTime);
 
 header('Location: day_records.php');
