e354e0a8fdf35fce35c52782b978724144e72e1c
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-05-03 20:50:01 -0300

    Part 2 - Clock In

--- src/controllers/innout.php
+++ src/controllers/innout.php
@@ -7,7 +7,12 @@ loadModel('WorkingHours');
 $user = $_SESSION['user'];
 $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
-$currentTime = date('H:i:s', time());
-$records->innout($currentTime);
 
+try {
+    $currentTime = date('H:i:s', time());
+    $records->innout($currentTime);
+    addSuccessMsg('Ponto inserido com sucesso!');
+} catch (AppException $e) {
+    addErrorMsg($e->getMessage());
+}
 header('Location: day_records.php');
