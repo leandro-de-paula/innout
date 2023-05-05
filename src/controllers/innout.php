6e42043881a30000b777b7efd20e3cc65f633c58
leandro-de-paula <leandrodepaula.ti@gmail.com>
2023-05-04 21:29:43 -0300

    Part 3 - Clock In

--- src/controllers/innout.php
+++ src/controllers/innout.php
@@ -10,6 +10,11 @@ $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
 try {
     $currentTime = date('H:i:s', time());
+
+    if($_POST['forcedTime']) {
+        $currentTime = $_POST['forcedTime'];
+    }
+    
     $records->innout($currentTime);
     addSuccessMsg('Ponto inserido com sucesso!');
 } catch (AppException $e) {
